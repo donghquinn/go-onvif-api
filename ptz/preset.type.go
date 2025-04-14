ae8cffc57bcf85b6009f8f37bf9fdee89186e675
Dong H. Kim <ehdgus1524@gmail.com>
2025-04-14 12:12:30 +0900

    prset controller

--- ptz/preset.type.go
+++ ptz/preset.type.go
@@ -7,6 +7,31 @@ type CreatePresetRequest struct {
 	PresetName   string `json:"presetName"`
 }
 
+type ApplyPresetRequest struct {
+	ProfileToken string  `json:"profileToken"`
+	PresetToken  string  `json:"presetToken"`
+	PanTiltX     float64 `json:"panTiltX"`
+	PanTiltY     float64 `json:"panTiltY"`
+	ZoomX        float64 `json:"zoomX"`
+	IsAbsolute   bool    `json:"isAbsoulte"`
+}
+
+type GetPresetListRequest struct {
+	ProfileToken string `json:"profileToken"`
+}
+
+// =========== RESPONSE
+type PresetListResponse struct {
+	Status  int                `json:"status"`
+	Code    string             `json:"code"`
+	Message string             `json:"message"`
+	Result  []onvif2.PTZPreset `json:"result"`
+}
+
 type GetPresetsResponse struct {
-	Preset []onvif2.PTZPreset
+	Preset []onvif2.PTZPreset `json:"preset"`
+}
+
+type GetPresetListResponse struct {
+	PresetList []onvif2.PTZPreset `json:"presetList"`
 }
